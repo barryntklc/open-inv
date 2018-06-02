a9fb57035b46e79d981f713830ee72c61061a075
spot2 <micro@hawaii.edu>
2018-06-01 14:05:19 -1000

    added form input, attempted to add konecty:mongo-counter but disk space full

--- client/main.js
+++ client/main.js
@@ -7,6 +7,39 @@ import { events } from '../imports/api/events.js';
 
 import './main.html';
 
+Template.body.events({
+  'submit .new-item'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+
+    //https://www.w3schools.com/js/js_dates.asp
+    var timestamp = new Date();
+    const type = target.Type.value;
+    const manufacturer = target.Manufacturer.value;
+    const model = target.Model.value;
+    const serial_primary = target.Serial_Primary.value;
+    const serial_secondary = target.Serial_Secondary.value;
+
+    // Insert a task into the collection
+    // Tasks.insert({
+    //   text,
+    //   createdAt: new Date(), // current time
+    // });
+    // console.log("[" + timestamp + ", " + type + "]");
+    console.log("[%s, %s, %s, %s]", timestamp, type, manufacturer, model);
+
+    // Clear form
+    //target.Type.value = 'Computer';
+    target.Manufacturer.value = '';
+    target.Model.value = '';
+    target.Serial_Primary.value = '';
+    target.Serial_Secondary.value = '';
+  },
+});
+
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
@@ -27,13 +60,16 @@ Template.hello.events({
 
 
 Template.body.helpers({
+  dbresults() {
+    return items.find({});
+  },
   items() {
-    return ITEM.find({});
+    return items.find({});
   },
   properties() {
-    return PROPERTY.find({});
+    return properties.find({});
   },
   events() {
-    return EVENTS.find({});
+    return events.find({});
   }
 });
